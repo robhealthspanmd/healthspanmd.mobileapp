54d322ecfd3bff23475407ee3fb47cebeddb98e2
robhealthspanmd <rob@healthspanmd.com>
2023-09-08 13:12:39 -0700

    Add project files.

---
+++ healthspanmd.mobileapp/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace healthspanmd.mobileapp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+           
+        }
+
+        async void OnLoginClicked(object sender, EventArgs e)
+        {
+            string username = usernameEntry.Text;
+            string password = passwordEntry.Text;
+
+            // Here you would typically validate the username and password against some service
+            if (username == "admin" && password == "password")
+            {
+                await Navigation.PushAsync(new Tracker());
+            }
+            else
+            {
+                await DisplayAlert("Failure", "Invalid username or password", "OK");
+            }
+        }
+    }
+}
---
+++ healthspanmd.mobileapp/Tracker.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace healthspanmd.mobileapp
+{
+    public partial class Tracker : ContentPage
+    {
+        public Tracker()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveDataClicked(object sender, EventArgs e)
+        {
+            string weight = weightEntry.Text;
+            string bloodPressure = bloodPressureEntry.Text;
+            string exerciseMinutes = exerciseMinutesEntry.Text;
+
+            // Here, you would typically store these values, possibly by sending them to a server
+            await DisplayAlert("Data Saved", "Your health data has been saved.", "OK");
+        }
+    }
+}
